38e0f3adfe7317dc86bd077328a3df51a9232e57
yomisosanya <98689579+yomisosanya@users.noreply.github.com>
2025-01-13 16:22:18 -0500

    CalendlyWidget removed

--- src/components/CalendlyButton.tsx
+++ src/components/CalendlyButton.tsx
@@ -10,6 +10,7 @@ interface State{
     isOpen: boolean;
 };
 
+
 export default function CalendlyButton(){
     const [state, setState] = useState<State>({isOpen: false});
     //const layout = {'background-color': '#0099ff', 'display': 'inline-flex'};
